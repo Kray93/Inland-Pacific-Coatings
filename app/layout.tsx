13c19f6bff2ed56a53d25395153ff6bc474663ec
Kevin Willig <kevinwillig@gmail.com>
2026-04-29 07:21:34 -0700

    Update layout.tsx
    add google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -25,9 +26,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://use.typekit.net/vat4zck.css" />
-      </head>
+        <link rel="stylesheet" href="https://use.typekit.net/vat4zck.css" />
+      </head>
       <body>
+        {/* Google Ads Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18126713560"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18126713560');
+          `}
+        </Script>
+
         <Header />
         {children}
         <Footer />
